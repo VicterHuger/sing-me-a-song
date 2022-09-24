d483ed91be812d885c3219f2a0dd97e58354222b
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-09-24 18:58:16 -0300

    test: add an integration test to /GET /recommendations route

--- back-end/tests/factories/recommendationFactory.ts
+++ back-end/tests/factories/recommendationFactory.ts
@@ -1,22 +1,31 @@
 import { faker } from '@faker-js/faker';
 import { Recommendation } from '@prisma/client';
+import { CreateRecommendationData } from '../../src/services/recommendationsService';
 import { prisma } from '../../src/database';
 
-function createRecommendation() {
+function createRecommendation(): CreateRecommendationData {
     return {
         name: faker.music.songName(),
-        youtubeLink: `https://www.youtube.com/watch?v=MPGmsRgqUVQ`,
+        youtubeLink: `https://www.youtube.com/watch?v=${faker.random.alphaNumeric(10)}`,
     };
 }
 
-async function insertRecommendation() {
-    const recommendation = {
-        name: faker.music.songName(),
-        youtubeLink: `https://www.youtube.com/watch?v=MPGmsRgqUVQ`,
-    }
-    return await prisma.recommendation.create({
-        data: recommendation
-    })
+async function insertRecommendation(count?: number): Promise<void | Recommendation> {
+
+    if (count === undefined) {
+        return await prisma.recommendation.create({
+            data: createRecommendation(),
+        });
+    };
+
+    const recommendations: CreateRecommendationData[] = Array.from({ length: count }).map(() => {
+        return createRecommendation();
+    });
+
+    await prisma.recommendation.createMany({
+        data: recommendations,
+        skipDuplicates: true,
+    });
 };
 
 async function findRecommendationByName(name: string) {
--- back-end/tests/integration/recommendation.test.ts
+++ back-end/tests/integration/recommendation.test.ts
@@ -2,16 +2,17 @@ import app from '../../src/app';
 import supertest from 'supertest';
 import { cleanDisconectTableFactory } from '../factories/cleanDisconectTableFactory';
 import { recommendationFactory } from '../factories/recommendationFactory';
+import { Recommendation } from '@prisma/client';
+import { faker } from '@faker-js/faker';
 
 beforeEach(async () => await cleanDisconectTableFactory.truncateRecommendationTable());
 
+afterAll(async () => {
+    await cleanDisconectTableFactory.truncateRecommendationTable();
+    await cleanDisconectTableFactory.disconectPrisma();
+});
 
 describe('/POST /recommendations', () => {
-
-    afterAll(async () => {
-        await cleanDisconectTableFactory.truncateRecommendationTable();
-        await cleanDisconectTableFactory.disconectPrisma();
-    });
 
     it('should return status 422 if an invalid schema is passed', async () => {
 
@@ -37,11 +38,44 @@ describe('/POST /recommendations', () => {
     });
 
     it('should return status 409, if the recommendation that is trying to be created has already been created', async () => {
-        const recommendation = await recommendationFactory.insertRecommendation();
+        const recommendation = await recommendationFactory.insertRecommendation() as Recommendation;
 
         const result = await supertest(app).post('/recommendations').send({ name: recommendation.name, youtubeLink: recommendation.youtubeLink });
 
         expect(result.status).toBe(409);
     })
 
+});
+
+describe('/GET /recommendations', () => {
+    it('should return an array with ten recommendations if there are more or equal ten recommendations in database', async () => {
+        await recommendationFactory.insertRecommendation(15) as void;
+
+        const result = await supertest(app).get('/recommendations');
+
+        expect(result.status).toBe(200);
+        expect(result.body.length).toBe(10);
+        expect(result.body[0]).toMatchObject(expect.objectContaining({
+            id: expect.any(Number),
+            name: expect.any(String),
+            youtubeLink: expect.any(String),
+            score: expect.any(Number)
+        }));
+    });
+
+    it('should return the exact recommendations number, if there is less than 10 recommendations on database', async () => {
+        const count = faker.mersenne.rand(1, 9);
+        await recommendationFactory.insertRecommendation(count);
+
+        const result = await supertest(app).get('/recommendations');
+
+        expect(result.status).toBe(200);
+        expect(result.body.length).toBe(count);
+        expect(result.body[0]).toMatchObject(expect.objectContaining({
+            id: expect.any(Number),
+            name: expect.any(String),
+            youtubeLink: expect.any(String),
+            score: expect.any(Number)
+        }));
+    });
 })
